99afb9ff03be9683eae3529ba1ab821a319f776b
Dmitriy Govorov <govorovdim@gmail.com>
2024-10-19 15:45:16 +0200

    add initial configs

---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+vim.opt.encoding = "utf-8"
+vim.opt.nu = true -- enable line numbers
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.autoindent = true -- auto indentation
---
+++ lua/plugins/options.lua
@@ -0,0 +1,5 @@
+vim.cmd("colorscheme kanagawa-wave") --set color theme
+
+--bufferline
+vim.opt.termguicolors = true
+require("bufferline").setup{}
